84f3870e69b5112af95cbd8c31d9f68e8740e8b4
martjn <mingdev77@gmail.com>
2024-01-26 15:27:01 +0200

    create di container

--- public/index.php
+++ public/index.php
@@ -2,9 +2,12 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$container = new Container();
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response) {
